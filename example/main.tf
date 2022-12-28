2e18bb86e1c07454d25d2372175fbb2b3a2cce17
Travis Saucier <travis.saucier@sourcefuse.com>
2022-12-28 14:10:25 -0500

    add example

---
+++ example/main.tf
@@ -0,0 +1,42 @@
+################################################################################
+## defaults
+################################################################################
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.control_tower_home_region
+}
+
+module "tags" {
+  source = "git::https://github.com/sourcefuse/terraform-aws-refarch-tags?ref=1.0.3"
+
+  environment = terraform.workspace
+  project     = "terraform-aws-refarch-control-tower-aft"
+
+  extra_tags = {
+    Example = "true"
+  }
+}
+
+################################################################################
+## control tower
+################################################################################
+module "aft" {
+  source = "../"
+
+  account_ids                        = var.account_ids
+  aft_vpc_cidr                       = var.aft_vpc_cidr
+  control_tower_home_region          = var.control_tower_home_region
+  terraform_backend_secondary_region = var.terraform_backend_secondary_region
+}
---
+++ example/outputs.tf
@@ -0,0 +1,99 @@
+################################################################################
+## accounts
+################################################################################
+output "account_ids" {
+  description = "Map of account IDs for each account created."
+  value       = module.aft.account_ids
+}
+
+################################################################################
+## repos
+################################################################################
+output "account_customizations_repo_branch" {
+  description = "VCS Account customizations repo branch"
+  value       = module.aft.account_customizations_repo_branch
+}
+
+output "account_customizations_repo_name" {
+  description = "VCS Account customizations repo name"
+  value       = module.aft.account_customizations_repo_name
+}
+
+output "account_provisioning_customizations_repo_branch" {
+  description = "VCS Account provisioning customizations repo branch"
+  value       = module.aft.account_provisioning_customizations_repo_branch
+}
+
+output "account_provisioning_customizations_repo_name" {
+  description = "VCS Account provisioning customizations repo name"
+  value       = module.aft.account_provisioning_customizations_repo_name
+}
+
+output "account_request_repo_branch" {
+  description = "VCS Account request repo branch."
+  value       = module.aft.account_request_repo_branch
+}
+
+output "account_request_repo_name" {
+  description = "VCS Account request repo name."
+  value       = module.aft.account_request_repo_name
+}
+
+output "global_customizations_repo_branch" {
+  description = "Global customizations repo branch."
+  value       = module.aft.global_customizations_repo_branch
+}
+
+output "global_customizations_repo_name" {
+  description = "Global customizations repo name."
+  value       = module.aft.global_customizations_repo_name
+}
+
+output "vcs_provider" {
+  description = "VCS Provider where the repos are configure for the different accounts."
+  value       = module.aft.vcs_provider
+}
+
+################################################################################
+## aft features
+################################################################################
+output "aft_feature_cloudtrail_data_events" {
+  description = "AFT feature \"CloudTrail data events\"."
+  value       = module.aft.aft_feature_cloudtrail_data_events
+}
+
+output "aft_feature_delete_default_vpcs_enabled" {
+  description = "AFT feature \"delete default vpcs enabled\"."
+  value       = module.aft.aft_feature_delete_default_vpcs_enabled
+}
+
+################################################################################
+## networking
+################################################################################
+output "aft_vpc_cidr" {
+  description = "AFT VPC assigned cidr."
+  value       = module.aft.aft_vpc_cidr
+}
+
+output "aft_vpc_private_subnet_cidrs" {
+  description = "AFT VPC private subnet 01 cidr."
+  value       = module.aft.aft_vpc_private_subnet_cidrs
+}
+
+output "aft_vpc_public_subnet_cidrs" {
+  description = "AFT VPC private subnet 01 cidr."
+  value       = module.aft.aft_vpc_public_subnet_cidrs
+}
+
+################################################################################
+## terraform
+################################################################################
+output "terraform_version" {
+  description = "Terraform version used for this configuration."
+  value       = module.aft.terraform_version
+}
+
+output "tf_backend_secondary_region" {
+  description = "Terraform backend secondary region."
+  value       = module.aft.tf_backend_secondary_region
+}
