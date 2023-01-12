0a71d6c7d115597be929f943ca3ae007b7eb1c9d
Travis Saucier <travis.saucier@sourcefuse.com>
2023-01-12 15:16:36 -0500

    remove unneeded variables from example

--- example/main.tf
+++ example/main.tf
@@ -39,10 +39,4 @@ module "aft" {
   aft_vpc_cidr                       = var.aft_vpc_cidr
   control_tower_home_region          = var.control_tower_home_region
   terraform_backend_secondary_region = var.terraform_backend_secondary_region
-
-  aft_framework_repo                       = var.aft_framework_repo
-  account_customizations_repo              = var.account_customizations_repo
-  account_provisioning_customizations_repo = var.account_provisioning_customizations_repo
-  account_request_repo                     = var.account_request_repo
-  global_customizations_repo               = var.global_customizations_repo
 }
